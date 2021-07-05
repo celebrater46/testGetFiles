a5fa0d968019e2755a639902710107bc5e7eb404
hideru <jota.16.stigma.world@gmail.com>
2021-07-06 01:11:24 +0900

    Could not display files in sub folders

--- main.php
+++ main.php
@@ -1,4 +1,9 @@
 <?php
 
-$result = glob('./img/*');
-var_dump($result);
+// Could not display file names in sub folders. Only file names in parent folder
+
+//$result = glob('./img/*'); // test1, test2...
+$result1 = glob('./img/test1/*');
+$result2 = glob('./img/test2/*');
+var_dump($result1);
+var_dump($result2);
